387ecac49428abee8441dd4f815b167d6ded16cb
Dialpi <diegoapirazan@gmail.com>
2024-09-19 22:49:53 -0500

    barra lateral

--- CorpFreshh-v2/Crud/index.php
+++ CorpFreshh-v2/Crud/index.php
@@ -11,20 +11,109 @@ if (!isset($_SESSION['correo'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio</title>
-    <link rel=" stylesheet" href="css/bootstrap.min.css">
+    <link rel=" stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/sytles2.css">
+    <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet" />
 </head>
-<div class="container">
-    <div class="row mt-3 justify-content-center">
-        <div class="col-6">
-            <h1 class="text-center">CRUD</h1>
+<body>
+<div class="wrapper">
+        <aside id="sidebar">
+            <div class="d-flex">
+                <button class="toggle-btn" type="button">
+                    <i class="lni lni-grid-alt"></i>
+                </button>
+                <div class="sidebar-logo">
+                    <a href="index.php">Corp Freshh</a>
+                </div>
+            </div>
+            <ul class="sidebar-nav">
+                <li class="sidebar-item">
+                    <a href="index.php" class="sidebar-link">
+                        <i class="lni lni-home"></i>
+                        <span>Inicio</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a href="#" class="sidebar-link">
+                        <i class="lni lni-agenda"></i>
+                        <span>Task</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a href="#" class="sidebar-link collapsed has-dropdown" data-bs-toggle="collapse"
+                        data-bs-target="#auth" aria-expanded="false" aria-controls="auth">
+                        <i class="lni lni-protection"></i>
+                        <span>Auth</span>
+                    </a>
+                    <ul id="auth" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
+                        <li class="sidebar-item">
+                            <a href="#" class="sidebar-link">Login</a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a href="#" class="sidebar-link">Register</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="sidebar-item">
+                    <a href="#" class="sidebar-link collapsed has-dropdown" data-bs-toggle="collapse"
+                        data-bs-target="#multi" aria-expanded="false" aria-controls="multi">
+                        <i class="lni lni-layout"></i>
+                        <span>Multi Level</span>
+                    </a>
+                    <ul id="multi" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
+                        <li class="sidebar-item">
+                            <a href="#" class="sidebar-link collapsed" data-bs-toggle="collapse"
+                                data-bs-target="#multi-two" aria-expanded="false" aria-controls="multi-two">
+                                Two Links
+                            </a>
+                            <ul id="multi-two" class="sidebar-dropdown list-unstyled collapse">
+                                <li class="sidebar-item">
+                                    <a href="#" class="sidebar-link">Link 1</a>
+                                </li>
+                                <li class="sidebar-item">
+                                    <a href="#" class="sidebar-link">Link 2</a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+                <li class="sidebar-item">
+                    <a href="#" class="sidebar-link">
+                        <i class="lni lni-popup"></i>
+                        <span>Notification</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a href="#" class="sidebar-link">
+                        <i class="lni lni-cog"></i>
+                        <span>Setting</span>
+                    </a>
+                </li>
+            </ul>
+            <div class="sidebar-footer">
+                <a href="cerrarsesion.php" class="sidebar-link">
+                    <i class="lni lni-exit"></i>
+                    <span>Cerrar Sesión</span>
+                </a>
+            </div>
+        </aside>
+        <div class="main p-3">
+        <h1 class="text-center">CRUD</h1>
             <h2 class="text-center">Bienvenid@ <?php echo $_SESSION['correo']?></h2>
             <div class="text-center">
                 <a href="cerrarsesion.php">Cerrar Sesión</a>
             </div>
-            
         </div>
     </div>
-</div>
+
+<script src="../js/bootstrap.bundle.min.js"></script>
+<script>
+    const hamBurger = document.querySelector(".toggle-btn");
+
+hamBurger.addEventListener("click", function () {
+  document.querySelector("#sidebar").classList.toggle("expand");
+});
+</script>
 </body>
 
 </html>
--- CorpFreshh-v2/Crud/login.php
+++ CorpFreshh-v2/Crud/login.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-if(isset($_SESSION['nombre'])){
-    header(header: 'Location.index.php');
+if(isset($_SESSION['correo'])){
+    header( 'Location.index.php');
 }
 header(header: "Cache-Control: no-cache, no-store, must-revalidate");
 header(header: 'Pragma:no-cache');
@@ -108,6 +108,7 @@ function verpassword()
       }
   }
 </script>
+
     <script src="./js/bootstrap.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
